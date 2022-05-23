bbd3c2f9e40e85e08dd22a3b0cd4eb5539842d84
ScrewUcaptain <basilebrusseau@gmail.com>
2022-05-24 01:29:24 +0200

    add ship image, the class to handle it and refactor the run_game method in the principal file

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,33 +1,47 @@
 import sys
 import pygame
 
+from Settings import Settings
+from modules.ship import Ship 
+
 class AlienInvasion:
     """Overall class to manage game assets and behavior"""
     
     def __init__(self):
         """Initialize the game, and create game ressources"""
         pygame.init()
+        self.settings = Settings()
         
-        self.screen = pygame.display.set_mode(size=(1200,800))
+        
+        self.screen = pygame.display.set_mode(
+            size=(self.settings.screen_width, self.settings.screen_heigth)
+            )
+        
         pygame.display.set_caption("Alien Invasion")
-        #Set the background color.
-        self.bg_color = (230, 230, 230)
+        self.ship = Ship(self)
     
     def run_game(self):
         """Start the main loop for the game"""
         while True:
             #Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            #Redraw the screen during each pass through the loop.
+            self._update_screen()
+            
+    def _check_events(self):
+        """Respond to keypress and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+    
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        #Make the most recently drawn screen visible.
+        pygame.display.flip()
 
-                #Redraw the screen during each pass through the loop.
-                self.screen.fill(self.bg_color)
-                
-                #Make the most recently drawn screen visible.
-                pygame.display.flip()
-    
-    
+        
 if __name__ == "__main__":
     #MAke a game instance, and run the game.
     ai = AlienInvasion()
