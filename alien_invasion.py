323e952e4f61612588553bec3e95e39e73d44f49
ScrewUcaptain <basilebrusseau@gmail.com>
2022-05-24 13:10:13 +0200

    -add movement to the ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,6 +25,7 @@ class AlienInvasion:
         while True:
             #Watch for keyboard and mouse events.
             self._check_events()
+            self.ship.update()
             #Redraw the screen during each pass through the loop.
             self._update_screen()
             
@@ -33,6 +34,18 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.type == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.type == pygame.K_LEFT:
+                    self.ship.moving_left = False
+            
+                
     
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
