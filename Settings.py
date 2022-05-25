2097cd7c410413e6a81772cc96b5c09ca5dbbcf5
ScrewUcaptain <basilebrusseau@gmail.com>
2022-05-25 13:58:20 +0200

    create fleet and add collisions bullet-aliens

--- Settings.py
+++ Settings.py
@@ -1,19 +1,25 @@
 class Settings:
     """A class to store all settings for Alien Invasion."""
-    def __init__(self) :
+
+    def __init__(self):
         """Initialize the game's settings"""
-        #Screen settings
+        # Screen settings
         self.screen_width = 1200
         self.screen_heigth = 800
-        self.bg_color = (230,230,230)
-        
-        #Ship settings
-        self.ship_speed = 0.8
-        
-        #setting bullets
-        self.bullet_speed = 0.5
-        self.bullet_width = 5
-        self.bullet_height = 16
-        self.bullet_color = (212,10,175)
+        self.bg_color = (230, 230, 230)
+
+        # Ship settings
+        self.ship_speed = 1.2
+
+        # setting bullets
+        self.bullet_speed = 1.5
+        self.bullet_width = 10
+        self.bullet_height = 25
+        self.bullet_color = (212, 10, 175)
         self.bullets_allowed = 5
-        
+
+        # Alien settings
+        self.alien_speed = 0.8
+        self.fleet_drop_speed = 10
+        # fleet direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1  # don't change this
